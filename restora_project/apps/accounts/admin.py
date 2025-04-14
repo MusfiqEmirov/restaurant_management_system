53f0f2e99e24e7e4e151f12a968efbb29b452c72
musfiqamirov888 <musfiq.emirov5@gmail.com>
2025-04-14 23:51:25 +0400

    accounts and menu apps for models created

--- restora_project/apps/accounts/admin.py
+++ restora_project/apps/accounts/admin.py
@@ -1,3 +1,25 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import User, Profile
+
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    # admin panelde cedvelde gorunecek sahe
+    list_display = ('username', 'email', 'role', 'is_active')
+    # roleelre gore filter
+    list_filter = ('role', 'is_active')
+
+    # search_fields ilə istifadeci adina ve mailne gore axtris
+    search_fields = ('username', 'email')
+
+
+@admin.register(Profile)
+class ProfileAdmin(admin.ModelAdmin):
+    # ladmin panelde cedvelde gorunecek sahe
+    list_display = ('user', 'address', 'loyalty_points')
+
+    # search_fields ilə istifadeci adina gore axtris.
+    search_fields = ('user__username',)
+
+
--- restora_project/apps/core/models.py
+++ restora_project/apps/core/models.py
@@ -1,3 +1,53 @@
+from time import timezone
 from django.db import models
+from django.contrib.auth import get_user_model
 
+
+class TimestampMixin(models.Model): # diger modellerde vaxt fieldi yazmag evezine burdan miras alacaq
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True # modelin abstract oldugunu gosterir
+
+
+#qeydleri silmek evezine bazada saxlayir
+class SoftDeleteMixin(models.Model):
+    # qeydin silinib silmediyini
+    is_deleted = models.BooleanField(default=False)
+    # silinme tarixini
+    deleted_at = models.DateTimeField(blank=True, null=True)
+
+    #qeydi berpa edir
+    def delete(self, *args, **kwargs):
+        self.is_deleted = True
+        self.deleted_at = timezone.now()
+        self.save()
+
+    def restore(self):
+        self.is_deleted = False
+        self.deleted_at = None
+        self.save()
+
+    class Meta:
+        abstract = True
+
+# admin ve iscilerin fealiyetini izlemek ucun
+class AuditMixin(models.Model):
+    created_by = models.ForeignKey(get_user_model,
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True,
+        related_name='%(class)s_created'
+        )
+    updated_by = models.ForeignKey(
+        get_user_model(),
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True,
+        related_name='%(class)s_updated'
+    )
+
+    class Meta:
+        abstract = True
 
