76e3a46a529c502a8bd1d13234a495ec3eacfad1
musfiqamirov888 <musfiq.emirov5@gmail.com>
2025-05-27 21:23:02 +0400

    test uploated to server

--- restora_project/restora_project/urls.py
+++ restora_project/restora_project/urls.py
@@ -11,15 +11,9 @@ urlpatterns = [
     path('restaurant/api/v1/schema/', SpectacularAPIView.as_view(), name='schema'),
     path('restaurant/api/v1/schema/swagger-ui/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
     path('restaurant/api/v1/schema/redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
-
-    path('accounts/', include('api.accounts_bridge_core.urls')),
-    path('notifications/', include('api.notifications_bridge_core.urls')),
-    path('menu/', include('api.menu_bridge_core.urls')),
-    path('orders/', include('api.orders_bridge_core.urls')),
-    path('staff/', include('api.staff_bridge_core.urls')),
-    path('customers/', include('api.customers_bridge_core.urls')),
+    
 ]
 
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
