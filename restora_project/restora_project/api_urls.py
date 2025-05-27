a110a47932b591c1cc4a6b719adfd1b003ae5acc
musfiqamirov888 <musfiq.emirov5@gmail.com>
2025-05-27 21:11:55 +0400

    urls dtatic error handlers

--- restora_project/restora_project/api_urls.py
+++ restora_project/restora_project/api_urls.py
@@ -1,4 +1,6 @@
 from django.urls import path, include
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path('accounts/', include('api.accounts_bridge_core.urls')),
@@ -8,3 +10,9 @@ urlpatterns = [
     path('staff/', include('api.staff_bridge_core.urls')),
     path('customers/', include('api.customers_bridge_core.urls')),
 ]
+
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
